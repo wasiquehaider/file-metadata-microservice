ce3142ca6c4f0a6d37e97e9627de8967b57a4845
Glitch (dew-liquid) <none>
2019-02-08 06:29:49 +0000

    🗽☎️ Checkpoint ./package.json:2841847/3867 ./server.js:2841847/289

--- server.js
+++ server.js
@@ -2,17 +2,25 @@
 
 var express = require('express');
 var cors = require('cors');
-
+var bodyParser = require('body-parser');
 // require and use "multer"...
+var multer = require('multer');
+
+var upload = multer({dest: 'upload/'})
 
 var app = express();
-
+app.use(bodyParser.json())
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
      res.sendFile(process.cwd() + '/views/index.html');
   });
+
+app.post('api/fileanalyse',upload.single('upfile') ,(req,res,next)=> {
+
+
+})
 
 app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
